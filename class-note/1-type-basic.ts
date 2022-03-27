536b30b039d172a9c52f604e17d50f332887015a
sugyinbrs <sgnglee@gmail.com>
2022-03-27 23:43:10 +0900

    Add: 기본 타입 - 튜플, 객체, boolean

--- class-note/1-type-basic.ts
+++ class-note/1-type-basic.ts
@@ -9,5 +9,23 @@ const num: number = 10;
 
 // TS Array 선언 (Array 대문자로 선언)
 const arr: Array<number> = [1, 2, 3];
-const heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10]; // 지정한 타입 외 입력 시 컴파일 상 빨간줄 확인 가능
-const items: number[] = [1,2,3]; // 배열 리터럴
+const heroes: Array<string> = ["Capt", "Thor", "Hulk", 10]; // 지정한 타입 외 입력 시 컴파일 상 빨간줄 확인 가능
+const items: number[] = [1, 2, 3]; // 배열 리터럴
+
+// TS 튜플
+const address: [string, number] = ["gangnam", 10]; // 배열의 특정 인덱스 혹은 순서에 타입을 부여하는 것
+
+// TS 객체
+const obj: object = {};
+// const person: object = {
+//   name: 'sue',
+//   age: 100
+// };
+const person: { name: string; age: number } = {
+  name: "sue",
+  age: 100,
+};
+// 위와 같이 객체에 구체적으로 타입 명시 가능
+
+// TS 진위값 (boolean)
+const show: boolean = true;
