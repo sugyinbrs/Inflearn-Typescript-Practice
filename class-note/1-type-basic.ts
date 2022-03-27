4e33fd616723332d1959332e3cf7e9f1be6b2518
sugyinbrs <sgnglee@gmail.com>
2022-03-27 23:33:04 +0900

    Add: 기본 타입 - 문자열, 숫자, 배열

---
+++ class-note/1-type-basic.ts
@@ -0,0 +1,13 @@
+// JS 문자열 선언
+// var str = "hello";
+
+// TS 문자열 선언
+const str: string = "hello";
+
+// TS 숫자열 선언
+const num: number = 10;
+
+// TS Array 선언 (Array 대문자로 선언)
+const arr: Array<number> = [1, 2, 3];
+const heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10]; // 지정한 타입 외 입력 시 컴파일 상 빨간줄 확인 가능
+const items: number[] = [1,2,3]; // 배열 리터럴
