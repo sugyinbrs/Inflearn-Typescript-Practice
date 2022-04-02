5aa2a6c763908d7e45d4e5f18ae4b28064757cd9
sugyinbrs <sgnglee@gmail.com>
2022-04-02 23:56:27 +0900

    Add: 타입 별칭과 인터페이스의 차이점

--- class-note/4_type-aliases.ts
+++ class-note/4_type-aliases.ts
@@ -22,3 +22,9 @@ type Todo = { id: string; title: string; done: boolean };
 function getTodo(todo: Todo) {
 
 }
+
+/*
+타입 별칭은 새로운 타입 값을 하나 생성하는 것이 아니라 정의한 타입에 대해 나중에 쉽게 참고할 수 있게 이름을 부여하는 것과 같다.
+
+타입 별칭과 인터페이스의 가장 큰 차이점은 타입의 확장 가능 / 불가능 여부다. 인터페이스는 확장(extends)이 가능한데 반해 타입 별칭은 확장이 불가능하다. 따라서 가능한한 type 보다는 interface 로 선언해서 사용하는 것이 좋다.
+*/
