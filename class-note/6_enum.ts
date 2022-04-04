b1761c9e8653cadd19b0633690787f28f412cc02
sugyinbrs <sgnglee@gmail.com>
2022-04-04 23:52:37 +0900

    Add: Enum 소개와 숫자형 Enum

---
+++ class-note/6_enum.ts
@@ -0,0 +1,17 @@
+/*
+enum(이넘)은 특정 값들의 집합을 의미하는 자료형이며,
+
+타입스크립트에서는 문자형 이넘과 숫자형 이넘을 지원함
+*/
+
+enum Shoes {
+  Nike,
+  Adidas
+}
+// 항목을 목록에 추가할 수록 숫자가 자동 증가함
+
+var myShoes = Shoes.Nike;
+console.log(myShoes); // 0이 출력됨
+
+var yourShoes = Shoes.Adidas;
+console.log(yourShoes); // 1이 출력됨
